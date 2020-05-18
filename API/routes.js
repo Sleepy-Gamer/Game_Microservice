457c370ce8010799d56caad78b55839f350634ae
Katherine <Sleepy-Gamer@users.noreply.github.com>
2020-05-18 17:23:31 +0100

    Delete routes.js

--- API/routes.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-var controller = require('./controller')
-
-module.exports = function(app) {
-    app.route('/games/report').get(controller.report);
-    app.route('/games/:id').get(controller.gameSummary);
-};
